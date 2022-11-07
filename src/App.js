79b8e229c3d15723b49f9aef06bd5644db1fb8a2
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-11-06 18:01:57 -0600

    Appjs & indexjs finished, working on components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PortfolioComponent from './components/PortfolioComponent';
 
+// This function is a functional component that helps us split the UI into distinct parts.
+// In this case, we are returning another component, <HelloReact/> from it.
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <PortfolioComponent />;
 }
 
 export default App;
---
+++ src/components/PortfolioComponent.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Navbar from "Navbar";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+import Footer from "Footer";
+
+// Components are responsible for rendering some part of an application's UI.
+// Components let us split the UI into independent, reusable pieces, and also think about each piece in isolation.
+function PortfolioComponent() {
+
+
+}
+
+
+
+export default PortfolioComponent;
+
+
+    // JSX is a syntax extension to JavaScript that looks like HTML.
+    // JSX is compiled by something called "Babel" into widely supported ES5 syntax.
+    // The curly braces allow us to pass values and expressions into our view.
