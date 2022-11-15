f1ba5e7424323536916dbc26560555f0bf7fb5ed
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-11-15 09:17:47 -0600

    added deployment

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,8 @@ export default function Project(props) {
             <div className="card project-card mb-5" style={{ backgroundImage: `url(${props.image})` }}>
                 <div className="card-body overlay">
                     <span>
-                        <h5 className="card-title text-body">{props.name}</h5>
-                        <p className="card-title text-body">{props.description}</p>
+                        <h4 className="card-title text-body">{props.name}</h4>
+                        <h6 className="card-title text-body">{props.description}</h6>
                         <a href={props.link} target="_blank" className="card-link text-body">{props.liveLink}</a>
                         <a href={props.github} style={{opacity: 1}} target="_blank" className="card-link text-body">{props.gitHubLink}</a>
                     </span>
