767ac65463b68904e1d84dad39b6e3a882e6af65
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-11-24 22:39:54 -0600

    about page working

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutPic from '../../img/northwestern_profile.jpg';
+import '../../css/about.css'
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
@@ -10,11 +11,11 @@ export default function About() {
                 <h2>
                     About Me
                 </h2>
-                <div>
-                    <img alt='mypicture' src={AboutPic}/>
-                    <div>
+                <div className='bioCont'>
+                    <img alt='mypicture' className='img-thumbnail   myPic' src={AboutPic}/>
+                    <div className='bioInfo'>
                         <p>
-                            Hi, my name is Ali Celikay.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quas inventore nisi praesentium. Itaque fugit maxime ullam aliquid quae odio, voluptatum necessitatibus repellendus, explicabo illo, alias accusantium asperiores dolorem exercitationem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quas inventore nisi praesentium. Itaque fugit maxime ullam aliquid quae odio, voluptatum necessitatibus repellendus, explicabo illo, alias accusantium asperiores dolorem exercitationem.
                         </p>
                     </div>
                 </div>
